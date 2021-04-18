37b128da2d240da22f2ae1a67663d5de3427c427
AntFlopGame <73836816+AntFlopGame@users.noreply.github.com>
2021-04-18 10:09:25 +0300

    add

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return (
+    <header><img src={"https://www.logodesign.net/images/illustration-logo.png"}/></header>
+)
+}
+
+export default Header;
